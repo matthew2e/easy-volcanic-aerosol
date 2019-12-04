30b2736a4bac829a1cbe282cc6b1f0d563645c79
Matthew Toohey <mtoohey@geomar.de>
2019-12-04 21:47:12 +0100

    fixed unit of estinction in eva_build_forcing_on_levels.f90

--- run/Testing/run_eva_Pinatubo_CMIP6.sh
+++ run/Testing/run_eva_Pinatubo_CMIP6.sh
@@ -3,6 +3,9 @@
 # EVA runscript
 
 EXP=Pinatubo
+SAVEDIR='output'
+
+cd ../..
 
 cat > eva_namelist << EOF
 &eva_input
@@ -15,14 +18,16 @@ cat > eva_namelist << EOF
  sulfate_end_year       = 2015
 /
 &aod_output_files
- aod_output_dir         = '.',
- aod_file_savename      = "eva_aod_${EXP}",
+ aod_output_dir         = '${SAVEDIR}',
+ aod_file_savename      = "eva_aod_${EXP}_cmip6",
+ grid_filename          = "./gridfiles/CMIP6_ECHAM6_gridfile.nc",
  aod_start_year         = 1960,
  aod_end_year           = 2010
 /
 &forcing_output_files
- forcing_output_dir     = '.',
- forcing_file_savename  = "eva_forcing_${EXP}",
+ forcing_output_dir     = '${SAVEDIR}',
+ forcing_file_savename  = "eva_forcing_${EXP}_cmip6",
+ grid_filename          = "./gridfiles/CMIP6_ECHAM6_gridfile.nc",
  forcing_start_year     = 1990,
  forcing_end_year       = 1995
 /
@@ -30,6 +35,6 @@ EOF
 
 ./eva_build_sulfate_file
 ./eva_build_forcing_files_cmip6
-./eva_build_aod_file
+#./eva_build_aod_file
 
 
